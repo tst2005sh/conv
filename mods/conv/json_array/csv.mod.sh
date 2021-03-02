ef5e64466a3eab396d8535e509a5c218a8eacaef
TsT <tst2005@gmail.com>
2021-03-02 10:36:13 +0100

    save change

--- mods/conv/json_array/csv.mod.sh
+++ mods/conv/json_array/csv.mod.sh
@@ -1,5 +1,12 @@
+Deps jq
+jq_array_to_csv() { jq -r '.[]|@csv'; }
 
-json_array_to_csv() {
-	RequireMod jq;
-	jq_array_to_csv;
-}
+#json_array_to_csv() {
+#	RequireMod jq;
+#	jq_array_to_csv;
+#}
+#jq_array_to_csv() { jq_stack option -r; jq_stack call '.[]|@csv'; }
+
+#jq_function_array_to_csv='def array_to_csv: .[]|@csv;'
+#jq_array_to_csv() { JQF_OPTIONS='-r' jqf 'array_to_csv'; }
+
