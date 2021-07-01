92c708616c481194cdda2db6b9c6e51ea00c69b8
TsT <tst2005@gmail.com>
2021-07-01 19:27:18 +0200

    json_dfnl to json_dfv

---
+++ mods/conv/json_dfnl/json_dfv.mod.sh
@@ -0,0 +1,13 @@
+
+json_dfnl_to_dfv() {
+	jq '
+	def dfnl_to_dfv:
+		group_by(.dir+.file)|
+		map({
+			"dir":(first|.dir),
+			"file":(first|.file),
+			"value": sort_by(.n)|map(.line)|join("\n")
+		})
+	;
+	dfnl_to_dfv';
+}
