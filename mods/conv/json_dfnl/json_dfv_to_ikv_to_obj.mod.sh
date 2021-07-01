539d737ec9e66823e798f6266fab73fe5d231366
TsT <tst2005@gmail.com>
2021-07-01 15:29:09 +0200

    json_dfnl to json_dfv_to_ikv_to_obj

---
+++ mods/conv/json_dfnl/json_dfv_to_ikv_to_obj.mod.sh
@@ -0,0 +1,20 @@
+
+json_dfnl_to_dfv_to_ikv_to_obj() {
+	jq '
+	def dfnl_to_dfv:
+		group_by(.dir+.file)|
+		map({
+			"dir":(first|.dir),
+			"file":(first|.file),
+			"value": sort_by(.n)|map(.line)|join("\n")
+		})
+	;
+	def dfv_to_ikv:
+		map({"id":.dir, "key":.file, "value":.value})
+	;
+	def ikv_to_obj:
+		map( {"id":(.id), (.key): (.value)} )
+		| group_by(.id) | map(add) | .[]
+	;
+	dfnl_to_dfv | dfv_to_ikv | ikv_to_obj';
+}
