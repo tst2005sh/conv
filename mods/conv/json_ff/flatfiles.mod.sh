ad11e1aa33081a2f2619200a92c8f6ef1c7148b3
TsT <tst2005@gmail.com>
2021-07-03 17:37:48 +0200

    bugfix

--- mods/conv/json_ff/flatfiles.mod.sh
+++ mods/conv/json_ff/flatfiles.mod.sh
@@ -1,7 +1,7 @@
 json_ff_to_flatfiles() {
 	jq -r '	if type=="array" then .[] else . end|
 		if .mkdir and .write and .in then
-			@sh "[ -d \(.mkdir) ] || mkdir -- \(.mkdir);echo \(.value|@base64)|base64 -d>\(.in)"
+			@sh "[ -d \(.mkdir) ] || mkdir -- \(.mkdir);echo \(.write|@base64)|base64 -d>\(.in)"
 		else @sh "echo ERROR;"
 		end
 	' |
