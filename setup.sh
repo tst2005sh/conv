7f29120310678b0f346452a11005e845fea48bc6
TsT <tst2005@gmail.com>
2021-07-05 16:25:20 +0200

    xml2json (no verifed)

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ cd deps
 git clone https://github.com/tst2005sh/jq-helpers
 [ -e "jq-mods" ] ||
 git clone https://github.com/tst2005sh/jq-mods
+#[ -e "xml2json" ] ||
+#git clone https://github.com/hay/xml2json
