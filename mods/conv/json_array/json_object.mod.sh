f47e70357db233224dc8236cc39b857054cd51ec
TsT <tst2005@gmail.com>
2021-07-05 16:46:49 +0200

    fix array_to_object

--- mods/conv/json_array/json_object.mod.sh
+++ mods/conv/json_array/json_object.mod.sh
@@ -1,7 +1,8 @@
 RequireMod jq_stack
 json_array_to_json_object() {
+	. "${scriptdir}/deps/jq-mods/lib/array_to_object.jq.lib.sh"
 	jq_stack init
-	jq_stack encfunction array_to_object
+	jq_stack envfunction array_to_object
 	jq_stack call array_to_object
 	jq_stack run
 }
