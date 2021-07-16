be6daaafbe9195b9254800d01a8961e8cffb0196
TsT <tst2005@gmail.com>
2021-07-16 12:26:27 +0200

    more deps

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
 set -e
 cd "$(dirname "$0")"
-cd deps
+cd deps && {
+
 [ -e "jq-helpers" ] ||
 git clone https://github.com/tst2005sh/jq-helpers
+
 [ -e "jq-mods" ] ||
 git clone https://github.com/tst2005sh/jq-mods
-#[ -e "xml2json" ] ||
-#git clone https://github.com/hay/xml2json
+
+[ -e "xml2json" ] ||
+git clone https://github.com/hay/xml2json
+
+[ -e json2yaml ] ||
+git clone https://github.com/redsymbol/json2yaml
+
+#[ -e yaml2json ] ||
+#git clone https://github.com/tst2005sh/yaml2json
+
+}
+
+# https://pypi.org/project/yq/
+# https://github.com/martinblech/xmltodict
+
