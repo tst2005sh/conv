c6597123dc85d742075b173a27fde2b64377c112
TsT <tst2005@gmail.com>
2021-07-22 12:20:06 +0200

    mods/jq_stack3: code cleanup

--- mods/jq_stack3.mod.sh
+++ mods/jq_stack3.mod.sh
@@ -1,8 +1,6 @@
-Deps jq
-#use_jq_stack3() {
-        JQ_STACK3_MODDIR="$scriptdir/deps/jq-mods/lib"
 
-	. "$scriptdir/deps/jq-helpers/lib/jq_stack3.lib.sh"
-	#jq_stack() { jq_stack3 "$@"; }
-	# jqf() { jq_stack modcall "$@"; }
-#}
+Deps jq || return 1 :# here return is valid because we source it from a function
+JQ_STACK3_MODDIR="$scriptdir/deps/jq-mods/lib"
+. "$scriptdir/deps/jq-helpers/lib/jq_stack3.lib.sh"
+#jq_stack() { jq_stack3 "$@"; }
+# jqf() { jq_stack modcall "$@"; }
