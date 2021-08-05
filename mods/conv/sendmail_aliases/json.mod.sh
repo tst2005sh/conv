083dbdae7f2f2ecbd5e194febc302c0d8008f3e1
TsT <tst2005@gmail.com>
2021-08-05 15:20:40 +0200

    sendmail_aliases/json update

--- mods/conv/sendmail_aliases/json.mod.sh
+++ mods/conv/sendmail_aliases/json.mod.sh
@@ -5,6 +5,19 @@ sendmail_aliases_to_json_deps() {
 sendmail_aliases_to_json() {
 	sendmail_aliases_to_json_deps || return 1
 	jq_stack3 init option -R precall '[.,inputs]'
-	jq_stack3 call 'map(if .!="" and (test("^#")|not) then split(" *: *";"g") | {"key":first , "value": (.[1:]|join(":")|split(", *";"g"))} else empty end)'
-	jq_stack3 run
+	jq_stack3 call 'map(select( .!="" and (test("^#")|not) ))'
+	jq_stack3 call 'map(split(" *: *";"g") | {"key":first , "value": (.[1:]|join(":")|split(", *";"g"))})'
+
+	case "$1" in
+	('(v1)'|"")
+	jq_stack3 call 'map(.value|=join(", "))'
+	;;
+	('(v2)')
+	:
+	;;
+	('(v3)')
+	jq_stack3 call 'map( (.key) as $key|.value[]|{"key":$key, "value":.} )'
+	;;
+	esac
+	jq_stack3 run deinit
 }
