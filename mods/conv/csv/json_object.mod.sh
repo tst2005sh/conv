86b6589d18e64a2cac0f4e4313bab46dda111abf
TsT <tst2005@gmail.com>
2021-08-13 12:35:35 +0200

    typo bug fix

--- mods/conv/csv/json_object.mod.sh
+++ mods/conv/csv/json_object.mod.sh
@@ -9,5 +9,5 @@ csv_to_json_object_deps() {
 }
 csv_to_json_object() {
 	csv_to_json_object_deps || return 1
-	"${scriptdir}/deps/csv2json/csv2json_object.py;
+	"${scriptdir}/deps/csv2json/csv2json_object.py"
 }
