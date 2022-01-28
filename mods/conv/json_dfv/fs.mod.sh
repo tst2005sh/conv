55e76101df95869a445de2426bc4b7192147024e
TsT <tst2005@gmail.com>
2022-01-28 17:23:29 +0100

    conv: json_dfv/fs(v2) an optimized try

--- mods/conv/json_dfv/fs.mod.sh
+++ mods/conv/json_dfv/fs.mod.sh
@@ -5,13 +5,40 @@ json_dfv_to_fs_deps() {
 }
 json_dfv_to_fs() {
 	json_dfv_to_fs_deps || return 1
+
+	case "$1" in
+	('(v1)'|"")
 	jq -r '	if type=="array" then .[] else . end|
 		if .dir and .file and .value then
 			.dir|= if startswith("/") then "."+. else . end |
 			@sh "[ -d \(.dir) ] || mkdir -- \(.dir);echo \(.value|@base64)|base64 -d>\("\(.dir//".")/\(.file)")"
 		else @sh "echo ERROR;"
 		end
-	' |
+	'
+	;;
+	('(v2)')
+	jq -r '	# require a array of entries
+		if type!="array" then error("input must be a array (of objects), found \(type)") else . end|
+
+		# change absolute path to relative
+		map( if .dir then .dir|= if startswith("/") then "."+. else . end else @sh "echo ERROR;" end)|
+
+		# process per directory
+		group_by(.dir)|
+
+		map(
+		(first|if .dir then .dir|@sh "[ -d \(.) ] || mkdir -- \(.)" else @sh "echo ERROR;" end),
+		(
+			.[]|
+			if .dir and .file and .value then
+				@sh "echo \(.value|@base64)|base64 -d>\("\(.dir//".")/\(.file)")"
+			else @sh "echo ERROR;"
+			end
+		)
+		)[]
+	'
+	;;
+	esac |
 	if ${CONV_WRITE:-false}; then
 		while read -r line; do
 			eval "$line";
