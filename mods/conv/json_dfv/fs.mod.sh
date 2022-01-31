521ca9fc23fe999211742bcd0820dde024d13006
TsT <tst2005@gmail.com>
2022-01-31 10:45:17 +0100

    conv: json_dfv/fs bug fix about error managment in case of missing attribute

--- mods/conv/json_dfv/fs.mod.sh
+++ mods/conv/json_dfv/fs.mod.sh
@@ -8,34 +8,42 @@ json_dfv_to_fs() {
 
 	case "$1" in
 	('(v1)'|"")
-	jq -r '	if type=="array" then .[] else . end|
-		if .dir and .file and .value then
-			.dir|= if startswith("/") then "."+. else . end |
-			@sh "[ -d \(.dir) ] || mkdir -- \(.dir);echo \(.value|@base64)|base64 -d>\("\(.dir//".")/\(.file)")"
-		else @sh "echo ERROR;"
-		end
+	jq -r '
+		if type=="array" then .[] else . end
+		| select(.dir and .file and .value)
+		| .dir|= if startswith("/") then "."+. else . end
+		| @sh "[ -d \(.dir) ] || mkdir -- \(.dir);echo \(.value|@base64)|base64 -d>\("\(.dir//".")/\(.file)")"
 	'
 	;;
 	('(v2)')
-	jq -r '	# require a array of entries
-		if type!="array" then error("input must be a array (of objects), found \(type)") else . end|
+	jq -r '
+		# require a array of entries
+		if type!="array" then error("input must be a array (of objects), found \(type)") else . end
 
 		# change absolute path to relative
-		map( if .dir then .dir|= if startswith("/") then "."+. else . end else @sh "echo ERROR;" end)|
+		| map(
+			select(.dir and .file and .value)
+			| .dir|= if startswith("/") then "."+. else . end
+		)
 
 		# process per directory
-		group_by(.dir)|
+		| group_by(.dir)
 
-		map(
+		| map(
 		(first|if .dir then .dir|@sh "[ -d \(.) ] || mkdir -- \(.)" else @sh "echo ERROR;" end),
 		(
-			.[]|
-			if .dir and .file and .value then
-				@sh "echo \(.value|@base64)|base64 -d>\("\(.dir//".")/\(.file)")"
-			else @sh "echo ERROR;"
-			end
+			.[]
+			| select(.dir and .file and .value)
+			| @sh "echo \(.value|@base64)|base64 -d>\("\(.dir//".")/\(.file)")"
 		)
 		)[]
+	'
+	;;
+	('(v1b)')
+	jq -r '
+		.[]
+		| select(.dir and .file and .value)
+		| @sh "[ -d \(.dir) ] || mkdir -- \(.dir);base64 -d >\("\(.dir//".")/\(.file)") <<@" + "\n\(.value|@base64)\n@"
 	'
 	;;
 	esac |
