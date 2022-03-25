7dbf4f5f160eae4f09126dee0834d9236b8b5472
TsT <tst2005@gmail.com>
2022-03-25 19:58:57 +0100

    conv: csv/json_array need a better fix

--- mods/conv/csv/json_array.mod.sh
+++ mods/conv/csv/json_array.mod.sh
@@ -1,14 +1,14 @@
 
 csv_to_json_array_deps() {
-	if [ ! -x "${scriptdir}/deps/csv2json/csv2json_array.py" ]; then
+	if [ ! -x "${scriptdir}/deps/csv2json/csv2ndjson_array.py" ]; then
 		echo >&2 "missing dependency 'csv2json'"
 		return 1
 	fi
 	Deps python2 || return 1
-	Deps "${scriptdir}/deps/csv2json/csv2json_array.py" || return 1
+	Deps "${scriptdir}/deps/csv2json/csv2ndjson_array.py" || return 1
 
 }
 csv_to_json_array() {
 	csv_to_json_array_deps || return 1
-	"${scriptdir}/deps/csv2json/csv2json_array.py"
+	"${scriptdir}/deps/csv2json/csv2ndjson_array.py" | jq -scM .
 }
