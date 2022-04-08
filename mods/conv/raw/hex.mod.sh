1bc6430101fa6bbf671ebb6f79ad3b9dd4e6df02
TsT <tst2005@gmail.com>
2022-04-08 16:08:10 +0200

    conv: raw/hex (use luajit by default if available)

--- mods/conv/raw/hex.mod.sh
+++ mods/conv/raw/hex.mod.sh
@@ -5,5 +5,23 @@ raw_to_hex_deps() {
 }
 raw_to_hex() {
 	raw_to_hex_deps || return 1
-	od -t x1 -An | tr -d -c '0-9a-fA-F'
+	if [ -z "$1" ] && command -v luajit >/dev/null 2>&1; then
+		set -- '(lua)'
+	fi
+	case "$1" in
+	(''|'(od+tr)')
+		od -t x1 -An | tr -d -c '0-9a-fA-F'
+	;;
+	('(lua)')
+		${LUA:-luajit} -e '
+		local I,O,v=io.stdin,io.stdout
+		while true do
+			v = I:read(1024);
+			if not v then break end
+			O:write( ("%0.2x"):rep(#v):format( string.byte(v,1,-1) ) )
+		end
+		'
+	;;
+	(*) echo >&2 ERROR:raw_to_hex; return 1;;
+	esac
 }
