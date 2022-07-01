40ac1e9439fb6f51b38aebc1699d24ca7afedbbd
TsT <tst2005@gmail.com>
2022-07-01 15:06:30 +0200

    conv: xml/json_via_xmltodict added

---
+++ mods/conv/xml/json_via_xmltodict.mod.sh
@@ -0,0 +1,14 @@
+xml_to_json_via_xmltodict_deps() {
+	local name=xml_to_json.via_xmltodict
+	local f="${scriptdir:-.}/deps/conv-py3.$name/bin/${name%%.*}.py3"
+	if [ ! -x "$f" ]; then
+		echo >&2 "missing dependency '${scriptdir:-.}/deps/conv-py3.$name'"
+		return 1
+	fi
+	Deps "$f" || return 1
+	DepsAny python3 || return 1
+}
+xml_to_json_via_xmltodict() {
+	xml_to_json_via_xmltodict_deps || return 1
+	"$(CommandAny python3)" "${scriptdir:-.}/deps/conv-py3.xml_to_json.via_xmltodict/bin/xml_to_json.py3" --strip_namespace
+}
